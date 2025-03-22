0e2ea3c513aad6eed2eb7f6c3046654fd629250a
Jacob Chang <jl55661688@JacobdeMacBook-Pro.local>
2025-03-22 16:27:17 +0800

    完成highlight

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { toAmount } from "@/utils/numberUtil";
 import { ArrowUpOutlined, ArrowDownOutlined } from "@ant-design/icons";
 import classNames from "classnames";
 import useLastPrice from "@/components/hooks/useLastPrice";
+import Decimal from "decimal.js";
 
 interface IUpdateTradeData {
   data: {
@@ -23,13 +24,12 @@ interface IUpdateTradeData {
 
 interface ITradeInfoList {
   latest: {
-    // Record<price, size>
-    asks: Record<string, string>;
-    bids: Record<string, string>;
+    asks: [string, string][];
+    bids: [string, string][];
   };
   previous: {
-    asks: Record<string, string>;
-    bids: Record<string, string>;
+    asks: [string, string][];
+    bids: [string, string][];
   };
 }
 
@@ -40,33 +40,83 @@ interface IDisplayTradeInfoList {
     bids: [string, string, string][];
   };
   previous: {
-    asks: Record<string, string>;
-    bids: Record<string, string>;
+    asks: [string, string][];
+    bids: [string, string][];
   };
 }
 
 const calculateTotal = (
   values: [string, string][]
 ): [string, string, string][] => {
-  let currentTotal = 0;
+  let currentTotal = new Decimal(0);
   return values.map((v) => {
-    currentTotal = (currentTotal) + Number(v[1]);
+    currentTotal = currentTotal.add(new Decimal(v[1]));
     return [v[0], v[1], currentTotal.toString()];
   });
 };
 
 const Home: React.FC = () => {
   const [tradeInfoList, setTradeInfoList] = useState<ITradeInfoList>({
-    latest: { asks: {}, bids: {} },
-    previous: { asks: {}, bids: {} },
+    latest: {
+      asks: [],
+      bids: [],
+    },
+    previous: {
+      asks: [],
+      bids: [],
+    },
   });
 
   const handleInitTradeInfoList = (result: IUpdateTradeData) => {
-    const initAsks = Object.fromEntries(result.data.asks);
-    const initBids = Object.fromEntries(result.data.bids);
-    setTradeInfoList({
-      latest: { asks: initAsks, bids: initBids },
-      previous: { asks: initAsks, bids: initBids },
+    const initAsks = result.data.asks.slice(0, 8);
+    const initBids = result.data.bids.slice(0, 8);
+    const newData = {
+      asks: initAsks,
+      bids: initBids,
+    };
+    setTradeInfoList({ latest: newData, previous: newData });
+  };
+
+  const handleUpdateTradeInfoList = (result: IUpdateTradeData) => {
+    setTradeInfoList((prev) => {
+      const currentInfoAsksObj = Object.fromEntries(prev.latest.asks);
+      const currentInfoBidsObj = Object.fromEntries(prev.latest.bids);
+
+      result.data.asks.forEach(([price, size]) => {
+        if (Number(size) === 0) {
+          delete currentInfoAsksObj[price];
+        } else {
+          currentInfoAsksObj[price] = size;
+        }
+      });
+
+      result.data.bids.forEach(([price, size]) => {
+        if (Number(size) === 0) {
+          delete currentInfoBidsObj[price];
+        } else {
+          currentInfoBidsObj[price] = size;
+        }
+      });
+
+      const newAsks = Object.entries(currentInfoAsksObj)
+        .sort(([aPrice], [bPrice]) => {
+          return Number(bPrice) - Number(aPrice);
+        })
+        .slice(0, 8);
+      const newBids = Object.entries(currentInfoBidsObj)
+        .sort(([aPrice], [bPrice]) => {
+          return Number(bPrice) - Number(aPrice);
+        })
+        .slice(0, 8);
+
+      const newData = {
+        asks: newAsks,
+        bids: newBids,
+      };
+      return {
+        latest: newData,
+        previous: prev.latest,
+      };
     });
   };
 
@@ -82,8 +132,11 @@ const Home: React.FC = () => {
     };
     ws.onmessage = (event) => {
       const result: IUpdateTradeData = SafeJSON.parse(event.data);
-      if (result.data.type === "snapshot") {
+      if (result.data?.type === "snapshot") {
         handleInitTradeInfoList(result);
+      }
+      if (result.data?.type === "delta") {
+        handleUpdateTradeInfoList(result);
       }
     };
 
@@ -96,24 +149,20 @@ const Home: React.FC = () => {
     };
   }, []);
 
-  const formattedDisplayTradeInfoList = useMemo<IDisplayTradeInfoList>(() => {
-    const latestAsksArray = Object.entries(tradeInfoList.latest.asks).slice(
-      0,
-      8
-    ).reverse();
-    const latestBidsArray = Object.entries(tradeInfoList.latest.bids).slice(
-      0,
-      8
-    );
-    return {
-      latest: {
-        asks: calculateTotal(latestAsksArray).reverse(),
-        bids: calculateTotal(latestBidsArray),
-      },
-      previous: tradeInfoList.previous,
-    };
-  }, [tradeInfoList]);
-
+  // tradeInfoList 更新太快所以節流優化
+  const formattedDisplayTradeInfoList =
+    useMemo<IDisplayTradeInfoList>((): IDisplayTradeInfoList => {
+      const latestAsksArray = tradeInfoList.latest.asks;
+      const latestBidsArray = tradeInfoList.latest.bids;
+      return {
+        ...tradeInfoList,
+        latest: {
+          // 賣出加總需反過來計算
+          asks: calculateTotal(latestAsksArray.reverse()).reverse(),
+          bids: calculateTotal(latestBidsArray),
+        },
+      };
+    }, [tradeInfoList]);
 
   // lastPrice
   const { lastPriceInfo } = useLastPrice();
@@ -139,27 +188,59 @@ const Home: React.FC = () => {
               {"Total"}
             </div>
           </div>
-          {formattedDisplayTradeInfoList.latest.asks.map((v) => {
-            const itemPrice = v[0];
-            const itemSize = v[1];
-            const total = v[2];
-            return (
-              <div
-                key={`asks_${itemPrice}_${itemSize}`}
-                className={styles.tradeInfoWrapper}
-              >
-                <div className={classNames(styles.priceCell, styles.sellColor)}>
-                  {toAmount({ value: itemPrice ,isFixed:true})}
+          {formattedDisplayTradeInfoList.latest.asks.map(
+            ([itemPrice, itemSize, total], index) => {
+              // Accumulative ratio
+              const asksList = formattedDisplayTradeInfoList.latest.asks;
+              const totalAll = asksList[0][2];
+              const ratio = (Number(total) / Number(totalAll)) * 100;
+              // is new or not
+              const previousValuesObj = Object.fromEntries(
+                formattedDisplayTradeInfoList.previous.asks
+              );
+              const hasShown = itemPrice in previousValuesObj;
+              const isSizeHigher =
+                Number(itemSize) > Number(previousValuesObj[itemPrice]);
+              const isSizeLower =
+                Number(itemSize) < Number(previousValuesObj[itemPrice]);
+
+              return (
+                <div
+                  key={`asks_${itemPrice}_${itemSize}_${total}_${index}`}
+                  className={classNames(
+                    styles.tradeInfoWrapper,
+                    !hasShown && styles.flashRedAnimation
+                  )}
+                >
+                  <div
+                    className={classNames(styles.priceCell, styles.sellColor)}
+                  >
+                    {toAmount({ value: itemPrice, isFixed: true })}
+                  </div>
+                  <div
+                    className={classNames(
+                      styles.textEnd,
+                      styles.sizeCell,
+                      hasShown && isSizeLower && styles.flashRedAnimation,
+                      hasShown && isSizeHigher && styles.flashGreenAnimation
+                    )}
+                  >
+                    {toAmount({ value: itemSize })}
+                  </div>
+                  <div className={classNames(styles.textEnd, styles.totalCell)}>
+                    {toAmount({ value: total })}
+                    <div
+                      className={classNames(
+                        styles.totalCellAccumulative,
+                        styles.sellAccumulativeBg
+                      )}
+                      style={{ width: `${ratio}%` }}
+                    />
+                  </div>
                 </div>
-                <div className={classNames(styles.textEnd, styles.sizeCell)}>
-                  {toAmount({ value: itemSize })}
-                </div>
-                <div className={classNames(styles.textEnd, styles.totalCell)}>
-                  {toAmount({ value: total })}
-                </div>
-              </div>
-            );
-          })}
+              );
+            }
+          )}
         </div>
         <div
           className={classNames(
@@ -184,27 +265,57 @@ const Home: React.FC = () => {
             </span>
           )}
         </div>
-        {formattedDisplayTradeInfoList.latest.bids.map((v) => {
-          const itemPrice = v[0];
-          const itemSize = v[1];
-          const total = v[2];
-          return (
-            <div
-              key={`bids_${itemPrice}_${itemSize}`}
-              className={styles.tradeInfoWrapper}
-            >
-              <div className={classNames(styles.priceCell, styles.buyColor)}>
-                {toAmount({ value: itemPrice, isFixed:true })}
+        {formattedDisplayTradeInfoList.latest.bids.map(
+          ([itemPrice, itemSize, total], index) => {
+            // Accumulative ratio
+            const bidsList = formattedDisplayTradeInfoList.latest.bids;
+            const totalAll = bidsList[bidsList.length - 1][2];
+            const ratio = (Number(total) / Number(totalAll)) * 100;
+            // is new or not
+            const previousValuesObj = Object.fromEntries(
+              formattedDisplayTradeInfoList.previous.bids
+            );
+            const hasShown = itemPrice in previousValuesObj;
+            const isSizeHigher =
+                Number(itemSize) > Number(previousValuesObj[itemPrice]);
+              const isSizeLower =
+                Number(itemSize) < Number(previousValuesObj[itemPrice]);
+
+            return (
+              <div
+                key={`bids_${itemPrice}_${itemSize}_${total}_${index}`}
+                className={classNames(
+                  styles.tradeInfoWrapper,
+                  !hasShown && styles.flashGreenAnimation
+                )}
+              >
+                <div className={classNames(styles.priceCell, styles.buyColor)}>
+                  {toAmount({ value: itemPrice, isFixed: true })}
+                </div>
+                <div
+                  className={classNames(
+                    styles.textEnd,
+                    styles.sizeCell,
+                    hasShown && isSizeLower && styles.flashRedAnimation,
+                    hasShown && isSizeHigher && styles.flashGreenAnimation
+                  )}
+                >
+                  {toAmount({ value: itemSize })}
+                </div>
+                <div className={classNames(styles.textEnd, styles.totalCell)}>
+                  {toAmount({ value: total })}
+                  <div
+                    className={classNames(
+                      styles.totalCellAccumulative,
+                      styles.buyAccumulativeBg
+                    )}
+                    style={{ width: `${ratio}%` }}
+                  />
+                </div>
               </div>
-              <div className={classNames(styles.textEnd, styles.sizeCell)}>
-                {toAmount({ value: itemSize })}
-              </div>
-              <div className={classNames(styles.textEnd, styles.totalCell)}>
-                {toAmount({ value: total })}
-              </div>
-            </div>
-          );
-        })}
+            );
+          }
+        )}
       </div>
     </div>
   );
